4dbc4f0f3dca71a84a7965564a37bdf798b87ba5
Elena Ladygina <Ladygina_ev@yahoo.com>
2023-03-05 15:58:00 +0700

    lesson_1 and lesson_2

---
+++ lesson_2/lesson_1/lesson_1.js
@@ -0,0 +1,15 @@
+// let height, weight
+// height = prompt ("Введите свой рост")
+// weight = prompt ("Введите свой вес")
+// alert(`Ваш BMI = ${weight/(height*height)}`)
+
+let a = 3
+let b = 2
+let c
+
+c = a
+a = b
+b = c
+
+console.log(a)
+console.log(b)
